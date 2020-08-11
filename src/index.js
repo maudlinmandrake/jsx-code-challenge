9c822cc1ec5b63c1b2aba8541d802944369111e2
Jenny Mikac <65610141+maudlinmandrake@users.noreply.github.com>
2020-08-11 10:25:02 -0400

    Complete code challenge Satisfy the requirements in the comments using what was discussed in the Intro to JSX lesson. Pretty easy challenge!

--- src/index.js
+++ src/index.js
@@ -2,3 +2,17 @@
 //It should display a h1 heading.
 //It should display an unordered list (bullet points).
 //It should contain 3 list elements.
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My favorite houseplants</h1>
+    <ul>
+      <li>Monstera</li>
+      <li>Snake plant</li>
+      <li>Pilea</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
